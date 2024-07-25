a71c54c6ba73871b554d8f3b16b630030356d326
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-07-25 16:54:32 -0300

    Incluido classe Projetos

---
+++ ControleDeProjetosProauto/Models/Projetos.cs
@@ -0,0 +1,97 @@
+﻿using ControleDeProjetosProauto.Setup.DataBase;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ControleDeProjetosProauto.Models
+{
+    public class Projetos
+    {
+        [Key]
+        public int PRONID { get; set; }
+        [Required]
+        public string PROSNPRO { get; set; }
+        [Required]
+        public DateTime PRODDATCAD {  get; set; }
+        [Required]
+        public string PROSDESPRO { get; set; }
+        public char PROCSTAT {get; set; }
+        public string PROSOBRIG { get; set; }
+
+    }
+
+    public interface IProjetosRepository
+    {
+        void Insert(Projetos projetos);
+        void Update(Projetos projetos);
+        void Delete(Projetos projetos);
+        Projetos GetByID(int id);
+        IEnumerable<Projetos> GetAll();
+    }
+
+    public class ProjetoRepository : IProjetosRepository
+    {
+        private readonly DAL<Projetos> _dal;
+
+        public void Insert(Projetos projetos)
+        {
+            _dal.AdicionarRegistro(projetos);
+        }
+
+        public void Update(Projetos projetos)
+        {
+            _dal.AtualizarRegistro(projetos);
+        }
+
+        public void Delete(Projetos projetos)
+        {
+            _dal.DeletaRegistro(projetos);
+        }
+
+        public Projetos GetByID(int id) 
+        {
+            return _dal.ObterRegistroPor(c => c.PRONID.Equals(id));
+        }
+
+        public IEnumerable<Projetos> GetAll()
+        {
+            return _dal.ObterRegistros();
+        }
+    }
+
+    public class ProjetoService
+    {
+        private readonly IProjetosRepository _projetoRepository;
+
+        public ProjetoService(IProjetosRepository projetoRepository)
+        {
+            _projetoRepository = projetoRepository;
+        }
+
+        public void Incluir(Projetos projeto)
+        {
+            _projetoRepository.Insert(projeto);
+        }
+
+
+        public void Alterar(Projetos projeto)
+        {
+            
+            _projetoRepository.Update(projeto);
+        }
+
+        public void Excluir(Projetos produto)
+        {
+            _projetoRepository.Delete(produto);
+        }
+
+        public Projetos GetByID(int id)
+        {
+            return _projetoRepository.GetByID(id);
+        }
+
+        public IEnumerable<Projetos> GetAll()
+        {
+            return _projetoRepository.GetAll();
+        }
+    }
+}
---
+++ ControleDeProjetosProauto/Setup/DataBase/ControleContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeProjetosProauto.Setup.DataBase
+{
+    internal class ControleContext : DbContext
+    {
+        private string _connectionString;
+        public ControleContext()
+        {
+            string dbDiretorio = Path.Combine(Directory.GetCurrentDirectory(), "db");
+            if (!Directory.Exists(dbDiretorio))
+            {
+                Directory.CreateDirectory(dbDiretorio);
+            }
+            string PathArquivo = Path.Combine(dbDiretorio, "System.db");
+
+            //_connectionString = $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={PathArquivo};Initial Catalog=ControleDeValidades;Integrated Security=True;Connect Timeout=30";
+            _connectionString = $"Data Source = {PathArquivo};Cache=Shared";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite(_connectionString).UseLazyLoadingProxies();
+
+    }
+}

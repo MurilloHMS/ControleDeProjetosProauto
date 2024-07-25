a8406cbeb35b3535f1ec07aa92f20f93629af649
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-07-25 17:18:18 -0300

    Criado tela de cadastro de projetos

---
+++ ControleDeProjetosProauto/Views/UC/Frm_CadastroProjeto_UC.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace ControleDeProjetosProauto.Views
+{
+    partial class Frm_CadastroProjeto_UC
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            groupBox2 = new GroupBox();
+            button2 = new Button();
+            button3 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(38, 416);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Inserir";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(16, 169);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(264, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(16, 55);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(62, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(16, 37);
+            label1.Name = "label1";
+            label1.Size = new Size(18, 15);
+            label1.TabIndex = 0;
+            label1.Text = "ID";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Location = new Point(328, 22);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(374, 457);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Descritivo";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(16, 92);
+            label2.Name = "label2";
+            label2.Size = new Size(98, 15);
+            label2.TabIndex = 0;
+            label2.Text = "Nome do Projeto";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(16, 110);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(264, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(183, 37);
+            label3.Name = "label3";
+            label3.Size = new Size(97, 15);
+            label3.TabIndex = 0;
+            label3.Text = "Data de Cadastro";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(183, 55);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(97, 23);
+            dateTimePicker1.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(16, 225);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(264, 166);
+            textBox3.TabIndex = 1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(16, 207);
+            label4.Name = "label4";
+            label4.Size = new Size(67, 15);
+            label4.TabIndex = 0;
+            label4.Text = "Obrigações";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(21, 34);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(337, 405);
+            textBox4.TabIndex = 1;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(16, 151);
+            label5.Name = "label5";
+            label5.Size = new Size(97, 15);
+            label5.TabIndex = 0;
+            label5.Text = "Status do Projeto";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(textBox3);
+            groupBox2.Controls.Add(dateTimePicker1);
+            groupBox2.Controls.Add(button3);
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(button1);
+            groupBox2.Controls.Add(comboBox1);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(textBox1);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(textBox2);
+            groupBox2.Controls.Add(label1);
+            groupBox2.Location = new Point(18, 22);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(304, 457);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(119, 416);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 3;
+            button2.Text = "Atualizar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(200, 416);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 3;
+            button3.Text = "Deletar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Frm_CadastroProjeto_UC
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Frm_CadastroProjeto_UC";
+            Size = new Size(705, 496);
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private ComboBox comboBox1;
+        private TextBox textBox1;
+        private Label label1;
+        private GroupBox groupBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private Label label4;
+        private Label label5;
+        private GroupBox groupBox2;
+        private Button button3;
+        private Button button2;
+    }
+}
---
+++ ControleDeProjetosProauto/Views/UC/Frm_FiltroProjetos_UC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControleDeProjetosProauto.Views.UC
+{
+    public partial class Frm_FiltroProjetos_UC : UserControl
+    {
+        public Frm_FiltroProjetos_UC()
+        {
+            InitializeComponent();
+        }
+    }
+}
